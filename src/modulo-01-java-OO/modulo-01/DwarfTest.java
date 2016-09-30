fd7f9338e992d047a2b47a21efee93d2770facc9
Unknown <jenifferkosta@hotmail.com>
2016-09-29 22:17:16 -0300

    test(Dwarf)
    Adicionar novo caso de teste, ajustar casos existentes.

--- src/modulo-01-java-OO/modulo-01/DwarfTest.java
+++ src/modulo-01-java-OO/modulo-01/DwarfTest.java
@@ -10,14 +10,14 @@ public class DwarfTest
     @Test
     public void dwarfNasceComQuantVidas(){
         // Arrange
-        Dwarf novoAnao = new Dwarf("Teste",2,5,2016);
+        Dwarf novoAnao = new Dwarf();
         // Assert e Act
         assertEquals(110,novoAnao.getQuantVida());
     }
     @Test 
     public void dwarfPerdeQuantVidas(){
         // Arrange
-        Dwarf novoAnao = new Dwarf("Teste",2,5,2016);
+        Dwarf novoAnao = new Dwarf();
         // Act
         novoAnao.perdeVida();
         assertEquals(100,novoAnao.getQuantVida());
@@ -25,16 +25,16 @@ public class DwarfTest
     @Test 
     public void dwarfPerdeVinteVidas(){
         // Arrange
-        Dwarf novoAnao = new Dwarf("Teste",2,5,2016);
+        Dwarf novoAnao = new Dwarf();
         // Act
-        for(int i=0;i<=20;i++)
+        for(int i=0;i<20;i++)
         novoAnao.perdeVida();
-        assertEquals(90,novoAnao.getQuantVida());
+        assertEquals(0,novoAnao.getQuantVida());
     }
     @Test
     public void dwarfNasceAno2016Mes05Dia12(){
         // Arrange
-        Dwarf novoAnao = new Dwarf("Teste",12,05,2016);
+        Dwarf novoAnao = new Dwarf();
         // Act
         //novoAnao.getNumeroSorte();
         assertEquals(101.0,novoAnao.getNumeroSorte(),0.001);
@@ -42,7 +42,8 @@ public class DwarfTest
     @Test
     public void dwarfNasceAnoNaoBissextoNomeSeixas(){
         // Arrange
-        Dwarf novoAnao = new Dwarf("Seixas",12,05,2019);
+        DataTerceiraEra novaData = new DataTerceiraEra(12,05,2019);
+        Dwarf novoAnao = new Dwarf("Seixas",novaData);
         // Act
         //novoAnao.getNumeroSorte();
         assertEquals(33.0,novoAnao.getNumeroSorte(),0.001);
@@ -50,7 +51,8 @@ public class DwarfTest
         @Test
     public void dwarfNasceAnoBissextoEQuantVidaEntre80E90(){
         // Arrange
-        Dwarf novoAnao = new Dwarf("Batman",12,05,2020);
+        DataTerceiraEra novaData = new DataTerceiraEra(12,05,2020);
+        Dwarf novoAnao = new Dwarf("Batman",novaData);
         novoAnao.perdeVida();
         novoAnao.perdeVida();
         // Act
@@ -60,7 +62,8 @@ public class DwarfTest
     @Test
     public void dwarfNasceAnoBissextoNomeMeireles(){
         // Arrange
-        Dwarf novoAnao = new Dwarf("Meireles",12,05,2024);
+        DataTerceiraEra novaData = new DataTerceiraEra(12,05,2024);
+        Dwarf novoAnao = new Dwarf("Meireles",novaData);
         // Act
         //novoAnao.getNumeroSorte();
         assertEquals(101.0,novoAnao.getNumeroSorte(),0.001);
@@ -68,10 +71,73 @@ public class DwarfTest
     @Test
     public void dwarfNasceAnoNaoBissextoNomeMeireles(){
         // Arrange
-        Dwarf novoAnao = new Dwarf("Meireles",12,05,2022);
+        DataTerceiraEra novaData = new DataTerceiraEra(12,05,2022);
+        Dwarf novoAnao = new Dwarf("Meireles",novaData);
         // Act
         //novoAnao.getNumeroSorte();
         assertEquals(33,novoAnao.getNumeroSorte(),0.001);
     }
-
-}
+    @Test
+    public void dwarfNasceVivo(){
+        Dwarf novoAnao = new Dwarf();
+        assertEquals(Status.VIVO,novoAnao.getStatus());
+    }
+    @Test
+    public void dwarfMorre(){
+        Dwarf novoAnao = new Dwarf();
+        for(int i=0;i<=10;i++)
+        novoAnao.perdeVida();
+        assertEquals(Status.MORTO,novoAnao.getStatus());
+    }
+    @Test
+    public void dwarfNaoFicaVidaNegativa(){
+      Dwarf novoAnao = new Dwarf();
+      for(int i=0;i<20;i++)
+      novoAnao.perdeVida();
+      assertEquals(0,novoAnao.getQuantVida());
+    }
+    @Test
+    public void ElfoMataDwarf(){
+      Elfo novoElfo = new Elfo("Jeniffer");
+      Dwarf novoAnao = new Dwarf();
+      novoElfo.atirarFlecha(novoAnao);
+      novoElfo.atirarFlecha(novoAnao);
+      novoElfo.atirarFlecha(novoAnao);
+      novoElfo.atirarFlecha(novoAnao);
+      novoElfo.atirarFlecha(novoAnao);
+      novoElfo.atirarFlecha(novoAnao);
+      novoElfo.atirarFlecha(novoAnao);
+      novoElfo.atirarFlecha(novoAnao);
+      novoElfo.atirarFlecha(novoAnao);
+      novoElfo.atirarFlecha(novoAnao);
+      novoElfo.atirarFlecha(novoAnao);
+      assertEquals(0,novoAnao.getQuantVida());
+    }
+    @Test
+    public void dwarfRecebe3Flechadas(){
+      Elfo novoElfo = new Elfo("Jeniffer");
+      Dwarf novoAnao = new Dwarf();
+      novoElfo.atirarFlecha(novoAnao);
+      novoElfo.atirarFlecha(novoAnao);
+      novoElfo.atirarFlecha(novoAnao);
+      assertEquals(80,novoAnao.getQuantVida());
+    }
+    @Test
+    public void dwarfRecebe11Flechadas(){
+      Elfo novoElfo = new Elfo("Jeniffer");
+      Dwarf novoAnao = new Dwarf();
+      novoElfo.atirarFlecha(novoAnao);
+      novoElfo.atirarFlecha(novoAnao);
+      novoElfo.atirarFlecha(novoAnao);
+      novoElfo.atirarFlecha(novoAnao);
+      novoElfo.atirarFlecha(novoAnao);
+      novoElfo.atirarFlecha(novoAnao);
+      novoElfo.atirarFlecha(novoAnao);
+      novoElfo.atirarFlecha(novoAnao);
+      novoElfo.atirarFlecha(novoAnao);
+      novoElfo.atirarFlecha(novoAnao);
+      novoElfo.atirarFlecha(novoAnao);
+      assertEquals(0,novoAnao.getQuantVida());
+    }
+    
+ }
