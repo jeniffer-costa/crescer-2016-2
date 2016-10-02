7762cf25b2ec0f3b5edefeb5b845578809c11b3e
Unknown <jenifferkosta@hotmail.com>
2016-10-02 18:19:48 -0300

    feat(inventario)
    Adicionar novos metodos (itemMaiorQuantidade, ordenarItens).

--- src/modulo-01-java-OO/modulo-01/Inventario.java
+++ src/modulo-01-java-OO/modulo-01/Inventario.java
@@ -16,4 +16,35 @@ public class Inventario
     public Item getItem(int index){
        return itens.get(index);
     }
+    public String getDescricoesItens(){
+        String descricoesItens = itens.get(0).getDescricao();
+
+        for(int i=0;i<itens.size();i++){
+            descricoesItens = itens.get(i).getDescricao() + ",";
+        }
+        return descricoesItens;
+    }
+
+    public Item itemMaiorQuantidade(){
+        Item maiorQuantidade = itens.get(0);
+
+        for(int i=0;i<itens.size();i++){
+            if(maiorQuantidade.getQuantidade() < itens.get(i).getQuantidade()){
+                maiorQuantidade = itens.get(i);
+            }
+        }
+        return maiorQuantidade;
+    }
+
+    public void ordenarItens(){     
+        for (int i = 0; i < itens.size(); i++) 
+        {
+            Item menorQuantidade = itens.get(i);
+            for (int j = i - 1; j >= 0 && itens.get(j).getQuantidade() > menorQuantidade.getQuantidade(); j--)
+            {
+                itens.set(j+1,itens.get(j));
+                itens.set(j,menorQuantidade);
+            }                       
+        }               
+    }
 }
