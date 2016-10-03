0f632db4f7f1f30ff907a87716ef549240fd0150
Unknown <jenifferkosta@hotmail.com>
2016-10-03 00:15:35 -0300

    test(Elfo)
    Adicionar novos casos de teste.

--- src/modulo-01-java-OO/modulo-01/Elfo.java
+++ src/modulo-01-java-OO/modulo-01/Elfo.java
@@ -36,11 +36,11 @@ public class Elfo{
         return inventario;
     }
     public String toString(){
-       boolean flechaNoSingular = inventario.getItem(0).getQuantidade() == 1;
+       boolean flechaNoSingular = inventario.getItem(1).getQuantidade() == 1;
        boolean experienciaNoSingular = this.experiencia == 0 || this.experiencia == 1;
        return String.format ("%s possui %d %s e %s %s de experiência.",
        this.nome,
-       inventario.getItem(0).getQuantidade(),
+       inventario.getItem(1).getQuantidade(),
        flechaNoSingular ? "flecha" : "flechas",
        this.experiencia,
        experienciaNoSingular ? "nível" : "níveis");
@@ -50,7 +50,7 @@ public class Elfo{
     public void atirarFlecha(Dwarf dwarf){
         boolean temFlecha = inventario.getItem(0).getQuantidade() > 0;
         if (temFlecha){
-        inventario.getItem(0).setQuantidade(inventario.getItem(0).getQuantidade()-1);
+        inventario.getItem(1).setQuantidade(inventario.getItem(1).getQuantidade()-1);
         experiencia++;
         dwarf.perdeVida();
        }
