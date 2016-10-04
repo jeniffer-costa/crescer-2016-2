59404a6893b264e4a45bd7e3ecee077651f5f286
Unknown <jenifferkosta@hotmail.com>
2016-10-04 00:49:15 -0300

    feat(Personagem): Criar classe e unir os métodos e atributos de (Dwarf e Elfo).

---
+++ src/modulo-01-java-OO/modulo-01/Personagem.java
@@ -0,0 +1,45 @@
+public class Personagem{
+    protected String nome;
+    protected int experiencia;
+    protected Inventario inventario;
+    protected Status status;
+    protected double quantVida;
+
+    public Personagem(String nome){
+        this.nome = nome;
+        this.status = status.VIVO;
+        this.inventario = new Inventario();
+    }
+
+    public void setNome(String n){
+        nome = n;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public int getExperiencia(){
+        return experiencia;
+    }
+
+    public Inventario getInventario(){
+        return inventario;
+    }
+
+    public Status getStatus(){
+        return status;
+    }
+        
+    public double getQuantVida(){
+        return quantVida;
+    }
+
+    public void adicionarItem(Item item){
+        inventario.adicionarItem(item);
+    }
+
+    public void removerItem(Item item){
+        inventario.removerItem(item);
+    }
+}
