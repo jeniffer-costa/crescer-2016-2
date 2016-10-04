b61f2f08f45e7f29a7c65be7bbb71378fce0b9f0
Unknown <jenifferkosta@hotmail.com>
2016-10-04 00:51:03 -0300

    refactor(IrishDwarf): Remover duplicações extendendo a classe Dwarf.

--- src/modulo-01-java-OO/modulo-01/IrishDwarf.java
+++ src/modulo-01-java-OO/modulo-01/IrishDwarf.java
@@ -1,13 +1,7 @@
 import java.util.ArrayList;
 
-public class IrishDwarf
+public class IrishDwarf extends Dwarf
 {
-    private int quantVida = 110;
-    private DataTerceiraEra dataNascimento;
-    private String nome;
-    private int experiencia;
-    private Status status;
-    private Inventario inventario;
 
     public IrishDwarf()
     {
@@ -19,42 +13,10 @@ public class IrishDwarf
 
     public IrishDwarf(String nome, DataTerceiraEra dataNascimento)
     {
-        inventario = new Inventario();
-        this.nome = nome;
-        this.dataNascimento = dataNascimento;
-        this.status = Status.VIVO;
-    }
+        super(nome,dataNascimento);
 
-    public void perdeVida(){
-        boolean estaMorto = status.equals(Status.MORTO);
-        if(!estaMorto){
-            double numeroSorte = getNumeroSorte();
-            if (numeroSorte < 0){
-                experiencia++;
-            }
-            if (numeroSorte > 100){
-                quantVida -= 10;
-                if (getQuantVida() < 0){
-                    quantVida = 0;
-                }
-            }
-            if (getQuantVida() == 0){
-                status = Status.MORTO;
-            }
-        }
-    }
-
-    public int getQuantVida(){
-        return quantVida;
-    }
-
-    public Status getStatus(){
-        return status;
     }
     
-    public Inventario getInventario(){
-        return inventario;
-    }
 
     public double getNumeroSorte(){
         double valorInicial = 101.0;
@@ -66,14 +28,6 @@ public class IrishDwarf
             valorInicial = (valorInicial * 33) % 100;
         }
         return valorInicial;
-    }
-
-    public void adicionarItem(Item item){
-        inventario.adicionarItem(item);
-    }
-
-    public void removerItem(Item item){
-        inventario.removerItem(item);
     }
 
     public void tentarSorte(){
