532ea705993de2aa53e413c7fdcfe0b2b959aa8b
Unknown <jenifferkosta@hotmail.com>
2016-10-06 01:08:22 -0300

    fix(ElfoNoturno): Correção do método atirarFlecha.

--- src/modulo-01-java-OO/modulo-01/ElfoNoturno.java
+++ src/modulo-01-java-OO/modulo-01/ElfoNoturno.java
@@ -6,14 +6,21 @@ public class ElfoNoturno extends Elfo
     public ElfoNoturno(String nome){
         super(nome);
     }
+    
+    public ElfoNoturno(String nome, int qtdFlechas) {
+        super(nome, qtdFlechas);
+    }
 
     public void atirarFlecha(Dwarf dwarf){
-        super.atirarFlecha(dwarf);
-        experiencia = experiencia + 2;
-        quantVida = quantVida - (0.05 * 100);
-        if (Math.abs(quantVida) == 0){
-           status = Status.MORTO;
+        if (status == Status.VIVO && getInventario().getItem(0).getQuantidade() > 0){
+            super.atirarFlecha(dwarf);
+            experiencia = experiencia + 2;
+            double ValorPercentual = getQuantVida() * 0.05;
+
+            quantVida = getQuantVida() - ValorPercentual;
+            if ((int)this.quantVida == 0){
+                status = Status.MORTO;
+            }
         }
     }
 }
-    
