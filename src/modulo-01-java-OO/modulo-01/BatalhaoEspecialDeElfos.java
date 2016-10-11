4b0f6308932c9e19b1d45730af8e71b762153984
Unknown <jenifferkosta@hotmail.com>
2016-10-11 01:07:07 -0300

    feat(BatalhaoEspecialDeElfos): Implementar método buscar por Status.

--- src/modulo-01-java-OO/modulo-01/BatalhaoEspecialDeElfos.java
+++ src/modulo-01-java-OO/modulo-01/BatalhaoEspecialDeElfos.java
@@ -1,25 +1,58 @@
-import java.util.*; 
- 
-public class BatalhaoEspecialDeElfos 
-{ 
-     private Map<Elfo,Elfo> elfosBatalhaoEspecial = new HashMap<Elfo,Elfo>(); 
- 
-    public BatalhaoEspecialDeElfos(){ 
-    } 
- 
-    public void alistar(Elfo elfo){ 
-        boolean podeAlistar = elfo instanceof ElfoVerde || elfo instanceof ElfoNoturno; 
-        if(podeAlistar){ 
-            elfosBatalhaoEspecial.put(elfo,elfo); 
-        } 
-    } 
-     
-    public Elfo[] getAlistados() { 
-        return elfosBatalhaoEspecial.values().toArray(new Elfo[elfosBatalhaoEspecial.size()]); 
-         
-    } 
-     
-    public Elfo buscar(String nome){ 
-      return elfosBatalhaoEspecial.get(nome); 
-    } 
-} 
+import java.util.*;
+
+public class BatalhaoEspecialDeElfos implements Exercito{
+    private HashMap<String, ArrayList<Elfo>> contingente;
+    private HashMap<Status, ArrayList<Elfo>> grupoPorStatus;
+
+    public BatalhaoEspecialDeElfos() {
+        contingente = new HashMap<>();
+        grupoPorStatus = new HashMap<>();
+    }
+
+    public Elfo[] getAlistados() {
+        ArrayList<Elfo> resultado = new ArrayList<>();
+        for (ArrayList<Elfo> listas : contingente.values()) {
+            resultado.addAll(listas);
+        }
+        return resultado.toArray(new Elfo[resultado.size()]);
+    }
+
+    public void alistar(Elfo elfo) {
+        boolean podeAlistar = elfo instanceof ElfoVerde || elfo instanceof ElfoNoturno;
+        if (podeAlistar) {
+            ArrayList<Elfo> elfosComEsteNome = contingente.get(elfo.getNome());
+            boolean aindaNaoTemElfoComEsteNome = elfosComEsteNome == null;
+            if (aindaNaoTemElfoComEsteNome) {
+                elfosComEsteNome = new ArrayList<>();
+                contingente.put(elfo.getNome(), elfosComEsteNome);
+            }
+            elfosComEsteNome.add(elfo);
+        }
+    }
+
+    public Elfo buscar(String nome) {
+        return contingente.containsKey(nome) ? contingente.get(nome).get(0) : null;
+    }
+
+    public void agruparPorStatus() {
+        grupoPorStatus.clear();
+        for (Map.Entry<String, ArrayList<Elfo>> par : contingente.entrySet()) {
+            for (Elfo elfo : par.getValue()) {
+                Status status = elfo.getStatus();
+                ArrayList<Elfo> elfosDoStatus = grupoPorStatus.get(status);
+                boolean aindaNaoAgrupeiPorEsteStatus = elfosDoStatus == null;
+                if (aindaNaoAgrupeiPorEsteStatus) {
+                    elfosDoStatus = new ArrayList<>();
+                    grupoPorStatus.put(status, elfosDoStatus);
+                }
+                elfosDoStatus.add(elfo);
+            }
+        }
+    }
+    
+    public ArrayList<Elfo> buscar(Status status) {
+        agruparPorStatus();
+        return grupoPorStatus.get(status);
+    }
+    
+}
