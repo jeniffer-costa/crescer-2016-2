eb0615f8ea5269cda5155a946a2f928dbdd4883b
Unknown <jenifferkosta@hotmail.com>
2016-10-11 01:27:07 -0300

    test(ElfoVerde): Correção de casos de teste.

--- src/modulo-01-java-OO/modulo-01/ElfoVerdeTest.java
+++ src/modulo-01-java-OO/modulo-01/ElfoVerdeTest.java
@@ -5,39 +5,50 @@ import org.junit.Test;
 
 public class ElfoVerdeTest
 {
-  @Test
-  public void elfoVerdeNasceCom100deVida(){
-     Elfo novoElfo = new Elfo("Superman");
-     assertEquals(100,novoElfo.getQuantVida(),0.001);
-    }
-    
-  @Test
-  public void elfoVerdeAtiraUmaFlechaEGanhaExperiencia(){
-     ElfoVerde novoElfo = new ElfoVerde("Superman");
-     novoElfo.atirarFlecha(new Dwarf());
-     assertEquals(2,novoElfo.getExperiencia());
-    }
-  
-    @Test
-  public void elfoVerdeAtiraDuasFlechasEGanhaExperiencia(){
-     ElfoVerde novoElfo = new ElfoVerde("Superman");
-     novoElfo.atirarFlecha(new Dwarf());
-     novoElfo.atirarFlecha(new Dwarf());
-     assertEquals(4,novoElfo.getExperiencia());
-    }
-    
-  @Test
-  public void elfoVerdeAtiraDezFlechasEGanhaExperiencia(){
-     ElfoVerde novoElfo = new ElfoVerde("Superman");
-     for(int i=0;i<10;i++){
-     novoElfo.atirarFlecha(new Dwarf());
-    }
-     assertEquals(20,novoElfo.getExperiencia());
+        @After
+    public void tearDown(){
+      System.gc();
     }
     
     @Test
-  public void elfoVerdeNaoAtiraFlechaGetExperiencia(){
-     ElfoVerde novoElfo = new ElfoVerde("Superman");
-     assertEquals(0,novoElfo.getExperiencia());
+    public void elfoVerdeNasceCom100deVida(){
+        Elfo novoElfo = new Elfo("Superman");
+        assertEquals(100,novoElfo.getQuantVida(),0.001);
+    }
+
+    @Test
+    public void elfoVerdeAtiraUmaFlechaEGanhaExperiencia(){
+        ElfoVerde novoElfo = new ElfoVerde("Superman");
+        novoElfo.atirarFlecha(new Dwarf());
+        assertEquals(2,novoElfo.getExperiencia());
+    }
+
+    @Test
+    public void elfoVerdeAtiraDuasFlechasEGanhaExperiencia(){
+        ElfoVerde novoElfo = new ElfoVerde("Superman",12);
+        novoElfo.atirarFlecha(new Dwarf());
+        novoElfo.atirarFlecha(new Dwarf());
+        assertEquals(4,novoElfo.getExperiencia());
+    }
+
+    @Test
+    public void elfoVerdeAtiraDezFlechasEGanhaExperiencia(){
+        ElfoVerde novoElfo = new ElfoVerde("Superman",12);
+        for(int i=0;i<10;i++){
+            novoElfo.atirarFlecha(new Dwarf());
+        }
+        assertEquals(20,novoElfo.getExperiencia());
+    }
+
+    @Test
+    public void elfoVerdeNaoAtiraFlechaGetExperiencia(){
+        ElfoVerde novoElfo = new ElfoVerde("Superman");
+        assertEquals(0,novoElfo.getExperiencia());
+    }
+
+    @Test
+    public void elfoVerdeGanhaExperienciaMesmoNaoTendoFlechas(){
+        ElfoVerde novoElfo = new ElfoVerde("Superman");
+        assertEquals(0,novoElfo.getExperiencia());
     }
 }
