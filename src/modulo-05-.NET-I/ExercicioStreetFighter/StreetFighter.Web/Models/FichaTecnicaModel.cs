aaec405dee9ce72af8bf4819c9b32ae164e3f9bd
Unknown <jenifferkosta@hotmail.com>
2016-11-02 22:14:16 -0200

    fix(FichaTecnicaModel) : Alterar a model para apenas conter os campos solicitados.

--- src/modulo-05-.NET-I/ExercicioStreetFighter/StreetFighter.Web/Models/FichaTecnicaModel.cs
+++ src/modulo-05-.NET-I/ExercicioStreetFighter/StreetFighter.Web/Models/FichaTecnicaModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,23 +8,19 @@ namespace StreetFighter.Web.Models
 {
     public class FichaTecnicaModel
     {
-        public String PrimeiraAparicao { get; set; }
-        public DateTime DataNascimento { get; set; }
+        public string Imagem { get; set; }
+        public String Nome { get; set; }
+        public String DataNascimento { get; set; }
         public int Altura { get; set; }
         public int Peso { get; set; }
-        public String Medidas { get; set; }
-        public String TipoSanguineo { get; set; }
-        public String HabilidadesEspeciais { get; set; }
-        public String Gosta { get; set; }
-        public String Desgosta { get; set; }
-        public String EstiloDeLuta { get; set; }
+
         public String Origem { get; set; }
-        public String FalaDeVitória { get; set; }
-        public String SSF2Nickname { get; set; }
-        public String SFA3Nickname { get; set; }
-        public String SF4Nickname { get; set; }
-        public String SFA3Stage { get; set; }
-        public String SF2Stage { get; set; }
+
         public String GolpesEspeciais { get; set; }
+
+        [DisplayName("Personagem Oculto")]
+        public bool PersonagemOculto { get; set; }
+
+        
     }
 }
