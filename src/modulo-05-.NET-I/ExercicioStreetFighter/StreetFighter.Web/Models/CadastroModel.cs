2f804256d8c8a3ca96edc366249c5e324469d3e1
Unknown <jenifferkosta@hotmail.com>
2016-11-02 22:15:22 -0200

    feat(CadastroModel) : Implementar o model Cadastro.

---
+++ src/modulo-05-.NET-I/ExercicioStreetFighter/StreetFighter.Web/Models/CadastroModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StreetFighter.Web.Models
+{
+    public class CadastroModel
+    {
+        [DisplayName("URL da Imagem:")]
+        public string Imagem { get; set; }
+
+        [Required]
+        [DisplayName("Nome:")]
+        public string Nome { get; set; }
+
+        [Required]
+        [DisplayName("Nascimento:")]
+        public string DataNascimento { get; set; }
+
+        [Required]
+        [DisplayName("Altura:")]
+        public int Altura { get; set; }
+
+        [Required]
+        [DisplayName("Peso:")]
+        public decimal Peso { get; set; }
+
+        [Required]
+        [DisplayName("Golpes Especiais:")]
+        public string GolpesEspeciais { get; set; }
+
+
+        [DisplayName("Personagem Oculto")]
+        public bool PersonagemOculto { get; set; }
+
+        [DisplayName("Origem:")]
+        public String Origem { get; set; }
+    }
+}
